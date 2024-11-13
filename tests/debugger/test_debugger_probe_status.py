5a41eeca76ef43f3a184926290ec21253d1c10ba
Oleg Pudeyev <code@olegp.name>
2024-11-13 13:52:32 -0500

    ruby di

--- tests/debugger/test_debugger_probe_status.py
+++ tests/debugger/test_debugger_probe_status.py
@@ -4,7 +4,7 @@
 
 import tests.debugger.utils as base
 
-from utils import weblog, scenarios, features, remote_config as rc, bug, context
+from utils import weblog, scenarios, features, remote_config as rc, bug, context, missing_feature
 
 
 @features.debugger
@@ -44,6 +44,7 @@ class Test_Debugger_Probe_Statuses(base._Base_Debugger_Test):
 
     @bug(context.library == "python@2.16.0", reason="DEBUG-3127")
     @bug(context.library == "python@2.16.1", reason="DEBUG-3127")
+    @missing_feature(context.library == 'ruby', reason='Not yet implemented')
     def test_probe_status_metric(self):
         self._assert()
 
@@ -53,6 +54,7 @@ class Test_Debugger_Probe_Statuses(base._Base_Debugger_Test):
 
         self._setup(probes)
 
+    @missing_feature(context.library == 'ruby', reason='Not yet implemented')
     def test_probe_status_span(self):
         self._assert()
 
@@ -64,6 +66,7 @@ class Test_Debugger_Probe_Statuses(base._Base_Debugger_Test):
 
     @bug(context.library == "python@2.16.0", reason="DEBUG-3127")
     @bug(context.library == "python@2.16.1", reason="DEBUG-3127")
+    @missing_feature(context.library == 'ruby', reason='Not yet implemented')
     def test_probe_status_spandecoration(self):
         self._assert()
 
